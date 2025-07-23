4104b22358e613e6bc534b7a52a38c665cd65235
Heitor <161908829+Heitor-M33n@users.noreply.github.com>
2025-07-23 08:52:52 -0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,1 +1,23 @@
+EXP_MULT = 1.2
 
+class Player:
+  def __init__(self) -> None:
+    self._health = 100
+    self._def = 3
+    self._speed = 3
+    self._level = 1
+    self._total_xp = 0
+    self._required_xp = 100
+
+  def gain_exp(self, xp: int) -> None:
+    if self._re
+
+  def level_up(self) -> None:
+    self.level
+
+  @property
+  def level(self) -> int:
+    return self._level
+
+  @level.setter
+  def level(self, increment) -> None: 
